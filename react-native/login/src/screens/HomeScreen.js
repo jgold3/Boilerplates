3640bf1fbce82fec7f12d68ba777ce11a9a08752
jgold3 <32280330+jgold3@users.noreply.github.com>
2021-06-13 13:36:21 -0400

    Mock Login Complete

--- react-native/login/src/screens/HomeScreen.js
+++ react-native/login/src/screens/HomeScreen.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button, View, Text } from 'react-native';
+import { getUsers } from '../api/mock';
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>HomeScreen</Text>
-      <Button title="Log out" onPress={() => navigation.navigate('Login')} />
-    </View>
-  );
+export default class HomeScreen extends Component {
+  state = {
+    users: [],
+    hasLoadedUsers: false,
+    userLoadErrMsg: '',
+  };
+
+  loadUsers = () => {
+    this.setState({ hasLoadedUsers: false, userLoadingErrorMessage: '' });
+    getUsers()
+      .then(res => this.setState({users: res.users, hasLoadedUsers: true}))
+      .catch(this.handleUserLoadErr);
+  };
+
+  handleUserLoadErr = (res) => {
+    if (res.error === 401) {
+      this.props.navigation.navigate('Login');
+    } else {
+      this.setState({hasLoadedUsers: false, userLoadErrMsg: res.message})
+    }
+  };
+
+  componentDidMount = () => {
+    this.loadUsers();
+  };  
+
+  render = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>HomeScreen</Text>
+        {this.state.users.map((user) => (
+          <Text key={user.email}>{user.email}</Text>
+        ))}
+        <Button title="Log out" onPress={() => this.props.navigation.navigate('Login')} />
+      </View>
+    );
+  };
 }
-
-export default HomeScreen;
